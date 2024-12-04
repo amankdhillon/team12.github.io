4f4b1eda220ed7eb3936d8153cb5499ef758569d
katiemoc <kmoc@ucsd.edu>
2024-12-03 19:34:04 -0800

    fix name storing and result pages

--- quizScript.js
+++
@@ -1,7 +0,0 @@
-// function to store the user's name for the results page
-function getName(event) {
-    event.preventDefault()
-    const name = document.getElementById('name').value;
-        localStorage.setItem('user', name);
-        window.location.href = 'quiz.html';
-}
